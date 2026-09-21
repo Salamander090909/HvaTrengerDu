fc22a25d4a64e9a9dbe5ead686d1aa5d5c273669
Sander <sandertennstrand@gmail.com>
2026-09-21 09:49:44 +0200

    app.js setup og head.js

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
-console.log("hello");
+const express = require('express'); //importert express
+const argon2 = require('argon2');
+
+const app = express(); //vi lager appen
+
+app.set("view engine", "ejs")
+app.use(express.urlencoded({extended: true}))
+app.use(express.static("public"));
+
+
+app.get("/", (req, res) => {
+    res.render("index");
+})
+
+
+
+
+
+
+
+app.listen(4000, () => {
+    console.log("http://localhost:4000")
+}); //vi kjører appen på port 4000
