3976374406bc0cadf4fd04adf93b4a4eb8842051
Mikaelf09 <mikaelfeldt2309@gmail.com>
2026-09-23 09:36:36 +0200

    merge conflict resolved, up to date with other branch

--- app.js
+++ app.js
@@ -1,23 +1,64 @@
-const express = require('express'); //importert express
+const express = require('express');
 const argon2 = require('argon2');
+const mongoose = require('mongoose');
 
-const app = express(); //vi lager appen
+const app2 = express(); //vi lager appen git
 
-app.set("view engine", "ejs")
-app.use(express.urlencoded({extended: true}))
-app.use(express.static("public"));
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    const app = express();
+app2.set("view engine", "ejs")
+app2.use(express.urlencoded({extended: true}))
+app2.use(express.static("public"));
 
+const mongodb = mongoose.connect("mongodb+srv://Salamander09:MAdWoRWLW87O7cMg@cluster0.9ajv5sh.mongodb.net/?appName=Cluster0")
 
-app.get("/", (req, res) => {
-    res.render("index");
+const Forslag = require('./models/Forslag');
+const User = require("./models/User")
+
+app2.get("/login",(req, res) => {
+app2res.render("login")
 })
 
+app2.get("/registrer",(req, res) => {
+    res.render("registrer")
+})
 
+app2.get("/", async (req, res) => {
+    const alleForslag = await Forslag.find().sort({ dato: -1 });
+    res.render("index", { alleForslag });
+});
 
+app2.post("/login", (req, res) => {
+    const {email, passord} = req.body;
+    res.send(`din epost og passord`)
+}) 
 
+app2.post("/registrer", async (req, res) => {
+    const {email, passord, gjentaPassord} = req.body;
+    if(passord !== gjentaPassord) {
+        res.send("passord og gjenta passord stemmer ikke overens")
+     } else {
 
+        const hash = await argon2.hash(passord);
 
+        const user = User.insertOne({
+            email, 
+            passord: hash
+        })
 
-app.listen(4000, () => {
+        console.log(user);
+
+        console.log(hash);
+
+        res.redirect("/login")
+    }
+})
+
+app2.post("/", async (req, res) => {
+    const nyttForslag = new Forslag({ tekst: req.body.forslag });
+    await nyttForslag.save();
+    res.redirect('/');
+});
+
+app2.listen(4000, () => {
     console.log("http://localhost:4000")
 }); //vi kjører appen på port 4000
